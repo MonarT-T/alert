3dcef4ff5af0155a5418c0890badc4bae45b110f
Wojciech Przybyła <w.przybyla@ambsoft.pl>
2017-12-19 14:35:45 +0100

    Dodana animacja

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ export default class Alert extends React.Component {
 
 	componentDidMount() {
 		if (this.props.result === '200') {
-	  		this.refs.notificator.success("Sukces", "Akcja zakończona pomyślnie.", 3000);
+	  		this.refs.notificator.success("Sukces", "Akcja zakończona pomyślnie.", 3500);
 		} else if (this.props.result === '201') {
-  			this.refs.notificator.error("Błąd", "Akcja nie powiodła się.", 3000); 
+  			this.refs.notificator.error("Błąd", "Akcja nie powiodła się.", 3500); 
 		}
 	}
 
