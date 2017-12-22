ae860f808ec3044fa9b7c685b2ca4fa0f6cfdbba
Wojciech Przybyła <w.przybyla@ambsoft.pl>
2017-12-22 13:36:44 +0100

    poprawiona animacja

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import Alert from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-var result= Math.random() < 0.5 ? '201' : '200';
 
-ReactDOM.render(<Alert result={result}/>, document.getElementById('root'));
+ReactDOM.render(<Alert result={Math.random() < 0.5 ? '201' : '200'}/>, document.getElementById('root'));
 registerServiceWorker();
